e36b5fb9cbac91b523f3670f3836af951594b46f
rangerisrael <israelalisoso1@gmail.com>
2024-07-25 17:12:36 +0800

    converted path to alias

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config:Config.InitialOptions = {
   testEnvironment:'jsdom',
   setupFilesAfterEnv:['<rootDir>/setup.ts'],
   moduleNameMapper:{
+    '@/(.*)$':'<rootDir>/src/$1',
     '\\.(css|scss)$':'identity-obj-proxy',
     '\\.(jpg|jpeg|webp|svg|png)$': '<rootDir>/file.config.js',
   }
--- src/__test__/app.test.tsx
+++ src/__test__/app.test.tsx
@@ -1,7 +1,8 @@
 //@ts-ignore
 import React from 'react'
 import {render,screen} from '@testing-library/react'
-import App from '../App';
+import App from '@/App';
+
 
 
 
